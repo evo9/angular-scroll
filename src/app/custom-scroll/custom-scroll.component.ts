0296e3ffe40fa521673f6073a004b77120acf321
Oleg Timchenko <evo9.81@gmail.com>
2022-07-12 14:25:09 +0300

    ui-scroll testing

--- src/app/custom-scroll/custom-scroll.component.ts
+++ src/app/custom-scroll/custom-scroll.component.ts
@@ -17,10 +17,8 @@ export class CustomScrollComponent implements OnInit, AfterViewInit {
   private _direction: 'up' | 'down';
   private _isChangesDetectionEnabled = true;
   private _scrollContainer: any;
-  private _oldScrollTop = 0;
   private _oldHeight: number;
   private _scrollTop: number;
-  private _isSpliceUp = false;
 
   constructor() { }
 
@@ -32,13 +30,18 @@ export class CustomScrollComponent implements OnInit, AfterViewInit {
     this._scrollContainer = this.dialogContainer.nativeElement;
 
     this.messageItems.changes.subscribe(() => {
-      if (this._isSpliceUp) {
-        const diff = this._oldHeight - this._scrollContainer.scrollHeight;
-        console.log(this._oldHeight, this._scrollContainer.scrollHeight, this._scrollTop, diff);
-        this._scrollContainer.scrollTop = this._scrollTop - diff;
-        this._isSpliceUp = false;
-        return;
-      }
+      // console.log(this._direction)
+      // if (this._isSpliceUp && this._direction === 'down') {
+      //   const diff = this._oldHeight - this._scrollContainer.scrollHeight;
+      //   console.log(this._oldHeight, this._scrollContainer.scrollHeight, this._scrollTop, diff);
+      //   this._scrollContainer.scrollTop = this._scrollTop - diff;
+      //   this._isSpliceUp = false;
+      //   setTimeout(() => {
+      //     this.isScrollDisabled = false;
+      //   }, 100);
+      //   return;
+      // }
+
       if (!this._isChangesDetectionEnabled) {
         this._isChangesDetectionEnabled = true;
         return;
@@ -47,62 +50,29 @@ export class CustomScrollComponent implements OnInit, AfterViewInit {
       if (this._direction === 'up') {
         const diff = this._scrollContainer.scrollHeight - this._oldHeight;
         this._scrollContainer.scrollTop = diff + this._scrollTop;
-        this.items.splice(-10);
+        // this.items.splice(-10);
+        setTimeout(() => {
+          this.isScrollDisabled = false;
+        }, 100);
       }
 
-      if (this._direction === 'down') {
-        // const message = this.messageItems.get(10);
-        // const position = message.nativeElement.getBoundingClientRect();
-        // const height = position.top - position.height - 50;
-        // console.log(height, this._scrollContainer.scrollTop);
-        //
-        // this.items.splice(0, 10);
-        //
-        // this._scrollContainer.scrollTop = this._scrollContainer.scrollTop + height;
-
-        this._oldHeight = this._scrollContainer.scrollHeight;
-        this._scrollTop = this._scrollContainer.scrollTop;
-        this._isChangesDetectionEnabled = false;
-        this._isSpliceUp = true;
-        this.items.splice(0, 10);
-      }
-
-      // if (this.items.length >= 40) {
-      //   this._isChangesDetectionEnabled = false
+      // if (this._direction === 'down') {
+      //   // const message = this.messageItems.get(10);
+      //   // const position = message.nativeElement.getBoundingClientRect();
+      //   // const height = position.top - position.height - 50;
+      //   // console.log(height, this._scrollContainer.scrollTop);
+      //   //
+      //   // this.items.splice(0, 10);
+      //   //
+      //   // this._scrollContainer.scrollTop = this._scrollContainer.scrollTop + height;
       //
-      //   if (this._direction === 'up') {
-      //     this.items.splice(-10);
-      //     return;
-      //   }
-      //
+      //   this._oldHeight = this._scrollContainer.scrollHeight;
+      //   this._scrollTop = this._scrollContainer.scrollTop;
+      //   // this._isChangesDetectionEnabled = false;
+      //   this._isSpliceUp = true;
+      //   this.items.splice(0, 10);
       // }
-
-      // this.isScrollDisabled = true;
     });
-
-    // setTimeout(() => {
-    //
-    //
-    //   // this.items.splice(-10);
-    //   //
-    //   // // const scrollTop = this._scrollContainer.scrollTop;
-    //   // // const scrollHeight = this._scrollContainer.scrollHeight;
-    //   //
-    //   // this.items = [...generateItems(this.items.length - 1, 10), ...this.items];
-    //   //
-    //   //
-    //   // setTimeout(() => {
-    //   //   const item = this.messageItems.get(10);
-    //   //   console.log()
-    //   //   item.nativeElement.scrollIntoView();
-    //   //
-    //   //
-    //   //   // const diff = this._scrollContainer.scrollHeight - scrollHeight;
-    //   //   //   console.log(scrollHeight, this._scrollContainer.scrollHeight, diff, scrollTop, this._scrollContainer.scrollTop);
-    //   //   // this._scrollContainer.scrollTop = scrollTop - diff;
-    //   //
-    //   // }, 1000);
-    // }, 5000);
   }
 
   public trackByIndexFn(index: number): number {
@@ -110,6 +80,9 @@ export class CustomScrollComponent implements OnInit, AfterViewInit {
   }
 
   public onScroll(): void {
+    if (this.isScrollDisabled) {
+      return;
+    }
     const scrollTop = this._scrollContainer.scrollTop;
     scrollTop > this._scrollTop ? this._onScrollDown() : this._onScrollUp();
     this._scrollTop = scrollTop;
@@ -118,19 +91,21 @@ export class CustomScrollComponent implements OnInit, AfterViewInit {
   private _onScrollUp(): void {
     this._direction = 'up';
     if (this._scrollTop <= 20) {
+      // this.isScrollDisabled = true;
       this._oldHeight = this._scrollContainer.scrollHeight;
       this.items = [...generateItems(this.items.length - 1, 10), ...this.items];
     }
   }
 
   private _onScrollDown(): void {
-    this._direction = 'down';
-    const position = this._scrollContainer.scrollTop + this._scrollContainer.offsetHeight;
-    const height = this._scrollContainer.scrollHeight;
-
-    if (position > height - 50) {
-      this.items = [...this.items, ...generateItems(this.items.length - 1)];
-    }
+    // this._direction = 'down';
+    // const position = this._scrollContainer.scrollTop + this._scrollContainer.offsetHeight;
+    // const height = this._scrollContainer.scrollHeight;
+    //
+    // if (position > height - 50) {
+    //   this.isScrollDisabled = true;
+    //   this.items = [...this.items, ...generateItems(this.items.length - 1)];
+    // }
   }
 
 }
--- src/app/ngx-ui-scroll/ngx-ui-scroll.component.ts
+++ src/app/ngx-ui-scroll/ngx-ui-scroll.component.ts
@@ -1,4 +1,13 @@
-import { AfterViewInit, Component, EventEmitter, OnInit } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  EventEmitter,
+  OnInit,
+  QueryList,
+  ViewChild,
+  ViewChildren,
+} from '@angular/core';
 import { Datasource, IDatasource } from 'ngx-ui-scroll';
 import { SizeStrategy } from 'vscroll';
 
@@ -10,6 +19,9 @@ import { generateItems } from '../utils/app.util';
   styleUrls: ['./ngx-ui-scroll.component.scss'],
 })
 export class NgxUiScrollComponent implements OnInit, AfterViewInit {
+
+  @ViewChild('dialogContainer', { static: false }) dialogContainer: ElementRef<HTMLDivElement>;
+  @ViewChildren('messageItems') messageItems: QueryList<ElementRef<HTMLDivElement>>;
 
   private _data = new EventEmitter<any[]>();
   private _items = [];
@@ -37,35 +49,13 @@ export class NgxUiScrollComponent implements OnInit, AfterViewInit {
 
   ngAfterViewInit() {
     const { adapter } = this.datasource;
-    //
-    //
-    //   this.items = this._generateItems(20);
-    //
-    // adapter.relax();
-    // adapter.insert({ items: this.items });
-    //
-    // // adapter.bof$.subscribe(index => {
-    // //   console.log(index);
-    // // })
-    //
-    // // const container = this.scrollContainer.nativeElement;
-    // //
-    // // setTimeout(() => {
-    // //   container.scrollTop = container.scrollHeight+200;
-    // // }, 200);
-    //
-    // adapter.isLoading$.subscribe(val => console.log(val));
-    //
     adapter.firstVisible$.subscribe(async (event) => {
       const { $index } = event;
       const { bufferInfo: { minIndex, maxIndex } } = adapter;
 
-      // console.log($index, adapter.bufferInfo);
-
       if ($index === minIndex && this._firstIndex > $index) {
         console.log('prepend');
         const items = generateItems(maxIndex);
-
         await adapter.relax();
         await adapter.prepend({
           items,
@@ -77,26 +67,53 @@ export class NgxUiScrollComponent implements OnInit, AfterViewInit {
 
     adapter.lastVisible$.subscribe(async (event) => {
       const { $index } = event;
-      const { bufferInfo: { minIndex, maxIndex } } = adapter;
-
-      console.log($index, adapter.bufferInfo);
+      const { bufferInfo: { maxIndex } } = adapter;
 
       if ($index === maxIndex && this._lastIndex < $index) {
         console.log('append');
-        const items = generateItems(maxIndex);
-
-        await adapter.relax();
-        await adapter.append({
-          items,
-        });
+        // const items = generateItems(maxIndex);
+        // await adapter.relax();
+        // await adapter.append({
+        //   items,
+        // });
       }
       //
       this._lastIndex = $index;
     });
 
+    // adapter.isLoading$.subscribe(isLoading => {
+    //   if (isLoading) {
+    //     this.messageItems.forEach(item => {
+    //       if (this._isVisible(item)) {
+    //         console.log(item.nativeElement.classList);
+    //       }
+    //     });
+    //   }
+    // });
+
+
     setTimeout(() => {
       this._items = generateItems(0, 20);
       this._data.emit(this._items);
     }, 200);
+  }
+
+  public onClickItem(index: number): void {
+    // const message = this.messageItems.get(index);
+    // const position = message.nativeElement.getBoundingClientRect();
+    //
+    // this.dialogContainer.nativeElement.scrollTop = position.top;
+    const message = this.messageItems.last;
+
+    this.dialogContainer.nativeElement.scrollTop = message.nativeElement.getBoundingClientRect().bottom;
+  }
+
+  private _isVisible(item: ElementRef): boolean {
+    const container = this.dialogContainer.nativeElement;
+    const scrollTop = container.scrollTop;
+    const clientHeight = container.clientHeight;
+    const position = item.nativeElement.getBoundingClientRect();
+
+    return position.top >= scrollTop && position.bottom <= scrollTop + clientHeight;
   }
 }
